ea7535c0052838946757e9448c9831ec896af662
RazVan2208 <razvan.ghisa@student.tdsb.on.ca>
2023-10-08 19:31:19 -0400

    So, I started the second question, added 2/5 class

--- QuestionOCT8-P1.java
+++ QuestionOCT8-P1.java
@@ -13,14 +13,12 @@ class Main {
         for(String i : strList){
             int convertedNTime = n * 1000;
             int b = -1;
-            while(b < strList.size()){
                 long currentTime = System.currentTimeMillis();
                 long timePassed = currentTime - startTime;
                 if(timePassed >= convertedNTime){
                     b += 1;
                     System.out.println(strList.get(b));
                     System.out.println("I waited " + timePassed + " ms!");
-                }
             }
         }
     }
---
+++ ShapeInterface.java
@@ -0,0 +1,16 @@
+// --------- QUESTION 2 -------- //
+// Create an interface called Shape. It has the two following methods:
+// 	double getArea() - Returns the area of the shape.
+//	double getPerimeter() - Returns the perimeter of the shape.
+//	int getNumOfEdges() - Returns the number of edges of the shape.
+//	int getNumOfCorners() - Returns the number of corners of the shape.
+// Create the classes Square, Rectangle, Circle, EquilateralTriangle,
+ //   RightAngledTriange and Parallelogram that implement the interface of Shape
+// 	- Note: The constructor for each of these classes should take in the minimum number of parameters to be able to specify a shape of that class of any size.
+
+interface Shape{
+    double getArea();
+    double getPerimeter();
+    int getNumOfEdges();
+    int getNumOfCorners();
+}
