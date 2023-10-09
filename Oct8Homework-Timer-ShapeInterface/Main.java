7fec0ed036b5c4352a7b8556aaa5e659b307050a
RazVan2208 <razvan.ghisa@student.tdsb.on.ca>
2023-10-09 10:32:21 -0400

    made sure everything good

--- Oct8Homework-Timer-ShapeInterface/Main.java
+++ Oct8Homework-Timer-ShapeInterface/Main.java
@@ -2,6 +2,19 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
+    public static void main(String[] args) {
     List<Shape> shapes = new ArrayList<Shape>();
-    shapes.add(new Circle());
+    shapes.add(new Circle((3)));
+    shapes.add(new Square(3,3));
+    shapes.add(new Rectangle(3, 2));
+    shapes.add(new Parallelogram(3, 2, 1));
+    shapes.add(new RightAngleTriange(3, 4, 5));
+    shapes.add(new EquilateralTriangle(2, 2));
+    for(Shape shapesI : shapes){
+        System.out.println(shapesI.getArea());
+        System.out.println(shapesI.getPerimeter());
+        System.out.println(shapesI.getNumOfEdges());
+        System.out.println(shapesI.getNumOfCorners());
+    }
+      }
 }
