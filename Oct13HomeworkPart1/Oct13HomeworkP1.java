ddb97e5a9ca75e047ce8f803fc6984e689b39ff8
RazVan2208 <razvan.ghisa@student.tdsb.on.ca>
2023-10-13 21:06:01 -0400

    Start of question, making Slope/yIntercept calculations and formulas

---
+++ Oct13HomeworkPart1/Oct13HomeworkP1.java
@@ -0,0 +1,17 @@
+package Oct13HomeworkPart1;
+// ------ QUESTION 1 ------- //
+// Given two points, figure out what line goes between those two points. 
+// Given a value x, return the corresponding y-value on the line you just found.
+// Example: x1=0, y1=0, x2=1, y2=1, x=2 -> Should return 2.
+public class Oct13HomeworkP1 {
+    static double lineInterpolate(double x1, double y1, double x2, double y2, double x){
+        double slopeOfLine;
+        double yIntercept;
+        slopeOfLine = (y2 - y1) / (x2 - x1);
+        yIntercept = 2;
+        return 2;
+    }
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+      }
+}
