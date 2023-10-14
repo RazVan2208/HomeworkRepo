c05c16b786121fdcd15333267a796bce5cacbcc8
RazVan2208 <razvan.ghisa@student.tdsb.on.ca>
2023-10-13 22:09:45 -0400

    all fixed

---
+++ Oct13HomeworkPart1/Oct13HomeworkP2.java
@@ -0,0 +1,19 @@
+package Oct13HomeworkPart1;
+// ------ QUESTION 2 ------- //
+// Create a class called OrderedPoints that stores a bunch of 2D points ordered by the x-values.
+// The constructor should take in no parameters.
+// The following public methods should be implemented:
+// 	void addPoint(double x, double y) - Adds a point
+//	List<Double> getXValues() - Returns a list of doubles, which are the x-values of all the added points, ordered from least to greatest
+//	List<Double> getYValues() - Returns a list of doubles, which are the y-values of all the added points, ordered by the corresponding x-values
+//	double getXValue(int i) - Returns the ith smallest x-value, where i is 0-indexed. (0 returns the smallest x-value)
+//	double getYValue(int i) - Returns the corresponding y-value for the ith-smallest x-value, where i is 0-indexed. (0 returns the corresponding y-value for the smallest x-value)
+//	void printPoints() - Prints each of the (x,y) point pairs, ordered by the x-values
+// Hint: When you add a point, insert it into the correct spot to keep the list(s) ordered. This will make it easier.
+
+public class Oct13HomeworkP2 {
+    public static void main(String[] args) {
+
+      }
+}
+
---
+++ Oct13HomeworkPart1/OrderedPoints.java
@@ -0,0 +1,11 @@
+package Oct13HomeworkPart1;
+import java.util.*;
+public class OrderedPoints {
+    ArrayList<Double> xPointList = new ArrayList<Double>();
+    ArrayList<Double> yPointList = new ArrayList<Double>();
+    public OrderedPoints(){
+       for(int i = 0; i < xPointList.size(); i += 1){
+        ArrayList<Double> mXPointList = new ArrayList<Double>(xPointList);
+       }
+    }
+}
