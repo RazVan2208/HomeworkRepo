8d24f50b23dfa9be8a49bdfc138e6ecc50c902c2
RazVan2208 <razvan.ghisa@student.tdsb.on.ca>
2023-10-13 21:25:48 -0400

    Done Q1

--- Oct13HomeworkPart1/Oct13HomeworkP1.java
+++ Oct13HomeworkPart1/Oct13HomeworkP1.java
@@ -7,11 +7,16 @@ public class Oct13HomeworkP1 {
     static double lineInterpolate(double x1, double y1, double x2, double y2, double x){
         double slopeOfLine;
         double yIntercept;
+        double missingY;
         slopeOfLine = (y2 - y1) / (x2 - x1);
-        yIntercept = 2;
-        return 2;
+        yIntercept = -(slopeOfLine * x1) + y1;
+        System.out.println(yIntercept);
+        String EquationOfTheLine = "The equation of the line is y = " + slopeOfLine + "x + " + yIntercept;
+        System.out.println(EquationOfTheLine);
+        missingY = slopeOfLine * x + yIntercept;
+        return missingY;
     }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(lineInterpolate(1, 3, 5, 7, 8));
       }
 }
