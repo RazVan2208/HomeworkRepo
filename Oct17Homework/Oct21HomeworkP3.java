31db258d97f5479c38ac94195d32d95359401230
RazVan2208 <razvan.ghisa@student.tdsb.on.ca>
2023-10-22 19:34:44 -0400

    Testing a different branch, added lyrics

--- Oct17Homework/Oct21HomeworkP3.java
+++ Oct17Homework/Oct21HomeworkP3.java
@@ -6,6 +6,7 @@ public class Oct21HomeworkP3 {
     static void PrintOutLyrics(double msOfSong){
         long startTime = System.currentTimeMillis();
         long lastTimePrinted = startTime;
+        boolean PrintedOnce = true;
         int i = 0;
         while(true){
             long currentTime = System.currentTimeMillis();
@@ -13,20 +14,44 @@ public class Oct21HomeworkP3 {
             if(timePassed > msOfSong){
                 break;
             }
+            
             long timeSinceLastPrint = currentTime - lastTimePrinted;
-            if(timePassed >= 30640 && timePassed <= 32640){
-                i = 1;
+            if(timePassed >= 30640 && timePassed <= 31640){
                 System.out.println("Life's like a road that you travel on");
                 timeSinceLastPrint = currentTime;
+
             }
-            i = 0;
-            if(timePassed >= 34890 && timePassed <= 37380 && i == 1){
-                System.out.println("Sometimes you bend and sometimes you stand");
+        
+            PrintedOnce = true;
+            if(timePassed >= 32640 && timePassed <= 33890){
+                System.out.println("When there's one day here and the next day gone");
                 timeSinceLastPrint = currentTime;
-                i = 0;
             }
 
-            
+            if(timePassed >= 34890 && timePassed <= 36380){
+                System.out.println("Sometimes you bend and sometimes you stand");
+                timeSinceLastPrint = currentTime;
+            }
+            if(timePassed >= 37380 && timePassed <= 38640){
+                System.out.println("Sometimes you turn your back to the wind");
+                timeSinceLastPrint = currentTime;
+            }
+            if(timePassed >= 39640 && timePassed <= 40890){
+                System.out.println("There's a world outside every darkened door");
+                timeSinceLastPrint = currentTime;
+            }
+            if(timePassed >= 41890 && timePassed <= 43140){
+                System.out.println("Where blues won't haunt you anymore");
+                timeSinceLastPrint = currentTime;
+            }
+            if(timePassed >= 44140 && timePassed <= 45390){
+                System.out.println("Where brave are free and lovers soar");
+                timeSinceLastPrint = currentTime;
+            }
+            if(timePassed >= 46390 && timePassed <= 47890){
+                System.out.println("Come ride with me to the distant shore");
+                timeSinceLastPrint = currentTime;
+            }
         }
     }
     public static void main(String[] args) {
