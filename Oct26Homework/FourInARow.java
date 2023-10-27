6b626de1f8b09648260eb03bb8ea73c5fa60a8c4
RazVan2208 <razvan.ghisa@student.tdsb.on.ca>
2023-10-26 22:49:24 -0400

    started FOurInARow

--- Oct26Homework/FourInARow.java
+++ Oct26Homework/FourInARow.java
@@ -1,5 +1,25 @@
 package Oct26Homework;
 
+import java.util.ArrayList;
+
 public class FourInARow {
-    
+    public ArrayList<ArrayList<Integer>> mFourInARowArr = new ArrayList<ArrayList<Integer>>();
+    public ArrayList<Integer> FirstRow = new ArrayList<Integer>();
+    public ArrayList<Integer> SecondRow = new ArrayList<Integer>();
+    public ArrayList<Integer> ThirdRow = new ArrayList<Integer>();
+    public ArrayList<Integer> FourthRow = new ArrayList<Integer>();
+    public ArrayList<Integer> FifthRow = new ArrayList<Integer>();
+    public ArrayList<Integer> FourthRow = new ArrayList<Integer>();
+
+    int mRow;
+    int mCol;
+
+
+    public FourInARow(int row, int col){
+        mRow = row;
+        mCol = col;
+    }
+    public void placeRed(){
+        
+    }
 }
