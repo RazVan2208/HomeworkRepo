ea0a582e02c4335201c3e9878c0194e41fc47cb9
RazVan2208 <razvan.ghisa@student.tdsb.on.ca>
2023-10-28 21:18:10 -0400

    Added an array system, trying to figure out why I cant input stuff

--- Oct26Homework/FourInARow.java
+++ Oct26Homework/FourInARow.java
@@ -3,23 +3,37 @@ package Oct26Homework;
 import java.util.ArrayList;
 
 public class FourInARow {
-    public ArrayList<ArrayList<Integer>> mFourInARowArr = new ArrayList<ArrayList<Integer>>();
-    public ArrayList<Integer> FirstRow = new ArrayList<Integer>();
-    public ArrayList<Integer> SecondRow = new ArrayList<Integer>();
-    public ArrayList<Integer> ThirdRow = new ArrayList<Integer>();
-    public ArrayList<Integer> FourthRow = new ArrayList<Integer>();
-    public ArrayList<Integer> FifthRow = new ArrayList<Integer>();
-    public ArrayList<Integer> FourthRow = new ArrayList<Integer>();
+    public char[][] playingGrid = new char[6][7];
+    public int mRow;
+    public int mCol;
 
-    int mRow;
-    int mCol;
-
-
-    public FourInARow(int row, int col){
-        mRow = row;
-        mCol = col;
+    public FourInARow(){
     }
-    public void placeRed(){
-        
+    public void placeRed(int row, int col){
+    mRow = row;
+    mCol = col;
+        if(mRow < 6 && mCol < 7){
+            playingGrid[row][col] = 'R';
+        }
+    }
+    public void placeYellow(int row, int col){
+    mRow = row;
+    mCol = col;
+        if(mRow < 6 && mCol < 7){
+            playingGrid[row][col] = 'Y';
+        }
+    }
+    public char[][] displayPlayGrid(){
+        int d = 0;
+        for(int i = 0; i < playingGrid[0].length; i += 1){
+            for(int j = 0; j < playingGrid.length; j += 1){
+                d += 1;
+                System.out.println(playingGrid[i][j]);
+                if(d % 3 == 0){
+                    System.out.println();
+                }
+            }
+        }
+        return playingGrid;
     }
 }
--- Oct26Homework/Main.java
+++ Oct26Homework/Main.java
@@ -1,5 +1,5 @@
 package Oct26Homework;
 
 public class Main {
-    
+    FourInARow newGame = new FourInARow();
 }
