9e27becac642944bba22a8e38e4da5621efaf90d
RazVan2208 <razvan.ghisa@student.tdsb.on.ca>
2024-01-05 11:48:17 -0500

    Test for mutliple keywords

--- Nov15Homework/Question5FileReaderByArg.java
+++ Nov15Homework/Question5FileReaderByArg.java
@@ -1,21 +1,33 @@
-package Nov15Homework;
-
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Scanner;
 
 // Create a Java program that takes at least two strings as arguments. The first argument is the absolute or relative path of a file. The rest of the arguments are words. For each word passed to the program, print out the number of times it appears in the file.
 public class Question5FileReaderByArg {
     public static void main(String[] args) {
     String filePath = "";
-    String test1 = "";
+    String keyWord = "";
     File ReadFile = new File(filePath);
     String absolutePath = ReadFile.getAbsolutePath();
     Scanner scan = null;
         for(String arg : args){
+            try {
+        scan = new Scanner(ReadFile);
+           while(scan.hasNextLine()){
+               System.out.println(scan.nextLine());
+           }
+        } catch (FileNotFoundException e){
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
            filePath = args[0].toString();
-           test1 = args[1].toString();
-           System.out.println(filePath);
-           System.out.println(test1);
+           for(int i = 1; i < args.length; i += 1){
+                keyWord = args[i];
+                System.out.println(keyWord);
+           }
         }
     }
 }
